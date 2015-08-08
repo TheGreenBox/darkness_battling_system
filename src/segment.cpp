de7a602354919a397387175859e638d9eca9b516
akindyakov <akindyakov@gmail.com>
2015-08-08 13:48:31 +0300

    add empty implementation for segment methods

--- src/segment.cpp
+++ src/segment.cpp
@@ -1,6 +1,25 @@
 #include "segment.h"
 
+#include <stdexcept>
+
 TSegment::TSegment(size_t row, size_t column)
     : Row(row)
     , Column(column) {}
 
+TSegment TSegment::Slide(EMoveOperations direction) const {
+    if (true) {
+        throw std::logic_error("Not implemented");
+    }
+    return TSegment(0, 0);
+}
+
+TSegment
+TSegment::RotateAround(
+    const TSegment& /*point*/,
+    EMoveOperations /*direction*/
+) const {
+    if (true) {
+        throw std::logic_error("Not implemented");
+    }
+    return TSegment(0, 0);
+}
