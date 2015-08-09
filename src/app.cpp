f59f80c57c200d24d5754850bfbb38518936a956
akindyakov <akindyakov@gmail.com>
2015-08-09 11:23:52 +0300

    Add debug out for source series

--- src/app.cpp
+++ src/app.cpp
@@ -2,41 +2,21 @@
  * g++(clang++) -std=c++11
  *
  */
+#include "exception.h"
+#include "game_session_rules.h"
+
 #include <getopt.h>
 
-#include <exception>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-
-class ArgparseException: public std::exception {
-public:
-    ArgparseException() {}
-    ArgparseException(const std::string& what) {
-        whatHappen.append(what);
-    }
-
-    template<typename T>
-    ArgparseException& operator<<(const T& val) {
-        std::ostringstream ostr;
-        ostr << val;
-        whatHappen.append(ostr.str());
-        return *this;
-    }
-
-    const char* what() const throw() override {
-        return whatHappen.c_str();
-    }
-
-private:
-    std::string whatHappen = "[argument parser exception] ";
-};
-
+#include <fstream>
 
 struct TArgs {
     std::vector<std::string> Filenames;
     std::vector<std::string> PhraseOfPower;
+    std::string DebugCmd;
     size_t TimeLimit = 0;
     size_t MemoryLimit = 0;
     size_t Cores = 1;
@@ -49,6 +29,7 @@ TArgs argparse(int argn, char** args) {
         {"memory-limit",    required_argument,  0,  'm' },
         {"cores",           required_argument,  0,  'c' },
         {"phrase",          required_argument,  0,  'p' },
+        {"debug",           required_argument,  0,  'd' },
         {"help",            no_argument,        0,  'h' },
         {0,                 0,                  0,   0 }
     };
@@ -56,35 +37,22 @@ TArgs argparse(int argn, char** args) {
     TArgs arguments;
     int option_index = 0;
     while (
-        int c = getopt_long(argn, args, "f:t:m:c:p:h:?", long_options, &option_index)
+        int c = getopt_long(argn, args, "f:t:m:c:p:h:d:?", long_options, &option_index)
     ) {
         if (c == -1) {
             break;
         } else if (c == 'f') {
-            if (!optarg) {
-                throw ArgparseException("empty value for option \'o\'");
-            }
             arguments.Filenames.push_back(optarg);
         } else if (c == 't') {
-            if (!optarg) {
-                throw ArgparseException("empty value for option \'o\'");
-            }
             arguments.TimeLimit = std::stoi(optarg);
         } else if (c == 'm') {
-            if (!optarg) {
-                throw ArgparseException("empty value for option \'o\'");
-            }
             arguments.MemoryLimit = std::stoi(optarg);
         } else if (c == 'c') {
-            if (!optarg) {
-                throw ArgparseException("empty value for option \'o\'");
-            }
             arguments.Cores = std::stoi(optarg);
         } else if (c == 'p') {
-            if (!optarg) {
-                throw ArgparseException("empty value for option \'o\'");
-            }
             arguments.PhraseOfPower.push_back(optarg);
+        } else if (c == 'p') {
+            arguments.DebugCmd = optarg;
         } else if (c == 'h' || c == '?') {
             std::cerr << "Usage:\n"
                 " -f  FILENAME    File containing JSON encoded input\n"
@@ -92,12 +60,14 @@ TArgs argparse(int argn, char** args) {
                 " -m  NUMBER  Memory limit, in megabytes, to produce output\n"
                 " -c  NUMBER  Number of processor cores available\n"
                 " -p  STRING  Phrase of power\n"
+                " -d  STRING  Debug mode\n"
                 " -h, -?, --help : Print this help\n"
             ;
             break;
         } else {
             if (0 != opterr) {
-                throw ArgparseException();
+                throw TException("Invalid configurating for argparse:")
+                    << " unknown option: -" << char(c);
             }
         }
     };
@@ -106,7 +76,31 @@ TArgs argparse(int argn, char** args) {
 
 int main(int argn, char** args) {
     try {
-        argparse(argn, args);
+        TArgs arguments = argparse(argn, args);
+
+        for (const auto& fname : arguments.Filenames) {
+            std::fstream fin(fname);
+            TGameSessionRules session(fin);
+            if (arguments.DebugCmd == "src") {
+                auto gameSet = session.NextSet();
+                for (const auto& u : gameSet) {
+                    for (const auto& seg : u.GetSegments()) {
+                        std::cout
+                            << "{ " << seg.GetRow()
+                            << ", " << seg.GetColumn()
+                            << " }, "
+                        ;
+                    }
+                    std::cout
+                        << " Pivot: { " << u.GetPivot().GetRow()
+                        << ", " << u.GetPivot().GetColumn()
+                        << " }, "
+                    ;
+                    std::cout << '\n';
+                }
+            }
+        }
+
     } catch (const std::exception& except) {
         std::cerr << except.what() << std::endl;
         return 1;
